34840959938e47e907d695aaa957b035a75dd00a
Renee Hosogi <reneehosogi@Renees-MacBook-Pro.local>
2015-12-03 17:05:36 -0800

    message about commit

--- diary14/Project_Draft.py
+++ diary14/Project_Draft.py
@@ -8,31 +8,47 @@ Created on Mon Nov 30 11:45:03 2015
 # Read in packages
 
 import pandas as pd
+import numpy as np
+import operator as o
 import matplotlib.pylab as plt
+import matplotlib.cm as cm
 
+# This will be how I combine quarterly data compiled by CE surveys
 # In Terminal, combine all four FMLD .csv files 
 
 # Renees-MacBook-Pro:diary14 reneehosogi$ cat < fmld141.csv <(tail +3 fmld142.csv) <(tail +3 fmld143.csv) <(tail +3 fmld144.csv) > veg14.csv
+# Renees-MacBook-Pro:diary13 reneehosogi$ cat < fmld131.csv <(tail +3 fmld132.csv) <(tail +3 fmld133.csv) <(tail +3 fmld134.csv) > veg13.csv
+# Renees-MacBook-Pro:diary12 reneehosogi$ cat < fmld121.csv <(tail +3 fmld122.csv) <(tail +3 fmld123.csv) <(tail +3 fmld124.csv) > veg12.csv
+# Renees-MacBook-Pro:diary11 reneehosogi$ cat < fmld111.csv <(tail +3 fmld112.csv) <(tail +3 fmld113.csv) <(tail +3 fmld114.csv) > veg11.csv
+# Renees-MacBook-Pro:diary10 reneehosogi$ cat < fmld101.csv <(tail +3 fmld102.csv) <(tail +3 fmld103.csv) <(tail +3 fmld104.csv) > veg10.csv
 
-# This will be how I 
 # Create dataframe
 
-veg = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/project/diary14/veg14.csv', sep=',')
+veg14 = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/Veg_Project/diary14/veg14.csv', sep=',')
+veg13 = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/Veg_Project/diary13/veg13.csv', sep=',')
+veg12 = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/Veg_Project/diary12/veg12.csv', sep=',')
+veg11 = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/Veg_Project/diary11/veg11.csv', sep=',')
+veg10 = pd.read_table('/Users/reneehosogi/Documents/GitHub_Clones/Veg_Project/diary10/veg10.csv', sep=',')
+low_memory=False
 
 # Test the read-in
-veg.head()
-veg.tail()
-veg.describe()
-veg.index
-veg.dtypes
-veg.shape
-veg.AGE_REF.describe()
-veg.FOODTOT.describe()
-veg.BEEF.value_counts()
+veg14.head()
+veg14.tail()
+veg13.describe()
+veg13.index
+veg12.dtypes
+veg12.shape
+veg11.PROD_UNIT.mean()
+veg11.FOODTOT.describe()
+veg10.BEEF.value_counts()
 
 # Check for missing data
 
-veg.FOODTOT.isnull().sum()
+veg14.FOODTOT.isnull().sum()
+veg13.FOODTOT.isnull().sum()
+veg12.FOODTOT.isnull().sum()
+veg11.FOODTOT.isnull().sum()
+veg10.FOODTOT.isnull().sum()
 
 # Parameters for graphs 
 
@@ -46,33 +62,119 @@ veg.plot(kind='scatter', x='AGE_REF', y='BEEF')
 veg.plot(kind='scatter', x='AGE_REF', y='POULTRY')
 veg.plot(kind='scatter', x='AGE_REF', y='PORK')
 
+# Converting obj into int 
+
+veg12.PORK.describe()
+
+veg12['BEEF']=veg12.BEEF.factorize()[0]
+veg12['PORK']=veg12.PORK.factorize()[0]
+veg12['POULTRY']=veg12.POULTRY.factorize()[0]
+veg12['OTHMEAT']=veg12.OTHMEAT.factorize()[0]
+veg12['SEAFOOD']=veg12.SEAFOOD.factorize()[0]
+
+veg12['FRSHFRUT']=veg12.FRSHFRUT.factorize()[0]
+veg12['FRSHVEG']=veg12.FRSHVEG.factorize()[0]
+veg12['PROCFRUT']=veg12.PROCFRUT.factorize()[0]
+veg12['PROCVEG']=veg12.PROCVEG.factorize()[0]
+
+veg11['BEEF']=veg11.BEEF.factorize()[0]
+veg11['PORK']=veg11.PORK.factorize()[0]
+veg11['POULTRY']=veg11.POULTRY.factorize()[0]
+veg11['OTHMEAT']=veg11.OTHMEAT.factorize()[0]
+veg11['SEAFOOD']=veg11.SEAFOOD.factorize()[0]
+
+veg11['FRSHFRUT']=veg11.FRSHFRUT.factorize()[0]
+veg11['FRSHVEG']=veg11.FRSHVEG.factorize()[0]
+veg11['PROCFRUT']=veg11.PROCFRUT.factorize()[0]
+veg11['PROCVEG']=veg11.PROCVEG.factorize()[0]
+
+veg10['BEEF']=veg10.BEEF.factorize()[0]
+veg10['PORK']=veg10.PORK.factorize()[0]
+veg10['POULTRY']=veg10.POULTRY.factorize()[0]
+veg10['OTHMEAT']=veg10.OTHMEAT.factorize()[0]
+veg10['SEAFOOD']=veg10.SEAFOOD.factorize()[0]
+
+veg10['FRSHFRUT']=veg10.FRSHFRUT.factorize()[0]
+veg10['FRSHVEG']=veg10.FRSHVEG.factorize()[0]
+veg10['PROCFRUT']=veg10.PROCFRUT.factorize()[0]
+veg10['PROCVEG']=veg10.PROCVEG.factorize()[0]
+
 # New columns for meat and produce numbers
 
-veg['MEAT']=veg.BEEF + veg.POULTRY + veg.PORK + veg.SEAFOOD + veg.OTHMEAT
-veg['PRODUCE']= veg.FRSHFRUT + veg.FRSHVEG + veg.PROCFRUT + veg.PROCVEG 
-veg['MEAT_UNIT']=
-veg['PROD_UNIT']=
+veg14['MEAT']=veg14.BEEF + veg14.POULTRY + veg14.PORK + veg14.SEAFOOD + veg14.OTHMEAT
+veg14['PRODUCE']= veg14.FRSHFRUT + veg14.FRSHVEG + veg14.PROCFRUT + veg14.PROCVEG 
+veg14['MEAT_UNIT']= (veg14.BEEF/5.71) + (veg14.POULTRY/2.04) + (veg14.PORK/3.83) + veg14.SEAFOOD + (veg14.OTHMEAT/3.01)
+veg14['PROD_UNIT']= (veg14.FRSHFRUT/1.49) + (veg14.FRSHVEG/1.57) + (veg14.PROCFRUT/2.53) +(veg14.PROCVEG/1.47)
 
+veg13['MEAT']=veg13.BEEF + veg13.POULTRY + veg13.PORK + veg13.SEAFOOD + veg13.OTHMEAT
+veg13['PRODUCE']= veg13.FRSHFRUT + veg13.FRSHVEG + veg13.PROCFRUT + veg13.PROCVEG 
+veg13['MEAT_UNIT']= (veg13.BEEF/4.76) + (veg13.POULTRY/2.02) + (veg13.PORK/3.43) + veg13.SEAFOOD + (veg13.OTHMEAT/3.05)
+veg13['PROD_UNIT']= (veg13.FRSHFRUT/1.29) + (veg13.FRSHVEG/1.49) + (veg13.PROCFRUT/2.51) +(veg13.PROCVEG/1.42)
+
+veg12['MEAT']=veg12.BEEF + veg12.POULTRY + veg12.PORK + veg12.SEAFOOD + veg12.OTHMEAT
+veg12['PRODUCE']= veg12.FRSHFRUT + veg12.FRSHVEG + veg12.PROCFRUT + veg12.PROCVEG 
+veg12['MEAT_UNIT']= (veg12.BEEF/4.57) + (veg12.POULTRY/1.97) + (veg12.PORK/3.37) + veg12.SEAFOOD + (veg12.OTHMEAT/2.92)
+veg12['PROD_UNIT']= (veg12.FRSHFRUT/1.34) + (veg12.FRSHVEG/1.42) + (veg12.PROCFRUT/2.66) + (veg12.PROCVEG/1.44)
+
+veg11['MEAT']=veg11.BEEF + veg11.POULTRY + veg11.PORK + veg11.SEAFOOD + veg11.OTHMEAT
+veg11['PRODUCE']= veg11.FRSHFRUT + veg11.FRSHVEG + veg11.PROCFRUT + veg11.PROCVEG 
+veg11['MEAT_UNIT']= (veg11.BEEF/4.34) + (veg11.POULTRY/1.89) + (veg11.PORK/3.35) + veg11.SEAFOOD + (veg11.OTHMEAT/3.15)
+veg11['PROD_UNIT']= (veg11.FRSHFRUT/1.33) + (veg11.FRSHVEG/1.58) + (veg11.PROCFRUT/2.76) + (veg11.PROCVEG/1.42)
+
+veg10['MEAT']=veg10.BEEF + veg10.POULTRY + veg10.PORK + veg10.SEAFOOD + veg10.OTHMEAT
+veg10['PRODUCE']= veg10.FRSHFRUT + veg10.FRSHVEG + veg10.PROCFRUT + veg10.PROCVEG 
+veg10['MEAT_UNIT']= (veg10.BEEF/5.26) + (veg10.POULTRY/1.96) + (veg10.PORK/3.10) + veg10.SEAFOOD + (veg10.OTHMEAT/3.22)
+veg10['PROD_UNIT']= (veg10.FRSHFRUT/1.32) + (veg10.FRSHVEG/1.15) + (veg10.PROCFRUT/2.47) +(veg10.PROCVEG/1.33)
+
+
+veg14.MEAT_UNIT.describe()
+veg12.MEAT.describe()
+veg12.MEAT_UNIT.describe()
+veg14.PRODUCE.describe()
 # Scatter plots for meat and produce consumption by dollar amount
 
-pd.scatter_matrix(veg[['MEAT', 'PRODUCE']])
+pd.scatter_matrix(veg[['MEAT_UNIT', 'PROD_UNIT']])
 pd.scatter_matrix(veg[['FOODHOME', 'FOODAWAY']])
 
 # Scatter plots for varying x-axis using meat data
 
-veg.plot(kind='scatter', x='FAM_SIZE', y='MEAT', title='Meat consumption by Family Size')
+veg14.plot(kind='scatter', x='FAM_SIZE', y='MEAT', title='Meat consumption by Family Size')
 plt.savefig('MEAT_FAM.png', dpi=1000)
 
-veg.plot(kind='scatter', x='AGE_REF', y='MEAT', title='Meat consumption by Age')
+veg14.plot(kind='scatter', x='AGE_REF', y='MEAT', title='Meat consumption by Age')
 plt.savefig('MEAT_AGE.png', dpi=1000)
 
-veg.plot(kind='scatter', x='REGION', y='MEAT', title='Meat consumption by Region')
+veg14.plot(kind='scatter', x='REGION', y='MEAT', title='Meat consumption by Region')
 plt.savefig('MEAT_REG.png', dpi=1000)
+
+veg14.plot(kind='scatter', x='FAM_SIZE', y='PRODUCE', title='Produce consumption by Family Size')
+plt.savefig('PROD_FAM.png', dpi=1000)
+
+veg14.plot(kind='scatter', x='AGE_REF', y='PRODUCE', title='Produce consumption by Age')
+plt.savefig('PROD_AGE.png', dpi=1000)
+
+veg14.plot(kind='scatter', x='REGION', y='PRODUCE', title='Produce consumption by Region')
+plt.savefig('PROD_REG.png', dpi=1000)
 
 # Other types of graphs for meats
 
-veg.hist(column='MEAT', by='FAM_SIZE')
-veg.MEAT.plot(kind='hist', bins=3)
+veg13.hist(column='MEAT_UNIT', by='REGION')
+veg.hist(column='PROD_UNIT', by='REGION')
+veg.MEAT_UNIT.plot(kind='hist', bins=3)
 
-# 
+# Comparison graphs
+veg10.MEAT_UNIT.mean()
+veg10.PROD_UNIT.mean()
 
+pd.scatter_matrix(veg[['MEAT_UNIT', 'PROD_UNIT']])
+
+vpoints = np.asarray([['MEAT_UNIT', '2014', 6.71544],
+           ['MEAT_UNIT', '2013', 6.7728],
+           ['MEAT_UNIT', '2012', 1605.3779],
+           ['MEAT_UNIT', '2011', 1610.1538]
+           ['MEAT_UNIT', '2010', 1645.05]
+           ['PROD_UNIT', '2014', 9.0523],
+           ['PROD_UNIT', '2013', 10.017],
+           ['PROD_UNIT', '2012', 1741.2795]
+           ['PROD_UNIT', '2011', 1732.67029]
+           ['PROD_UNIT', '2010', 2000.2411]])
